763271d47d7365dfd06d89a79f2a1fb97207f334
lw <root@fsoc.space>
2019-05-12 18:49:39 +0200

    Add tests

--- se1/07/src/ComplexNumbers.java
+++ se1/07/src/ComplexNumbers.java
@@ -1,6 +1,14 @@
 class ComplexNumbers {
     public static void main(String[] args) {
+        Complex c1 = new Complex();
+        Complex c2 = new Complex(0, 0);
+        Complex c3 = new Complex(1, -2);
 
+        Out.format("c1: %s, c2: %s, c3: %s%n", c1, c2, c3);
+        Out.format("c1 == c2, %s%n", c1 == c2 ? "true" : "false");
+        Out.format("c2 == c3, %s%n", c2 == c3 ? "true" : "false");
+        Out.format("c1 + c2 = %s%n", c1.add(c2));
+        Out.format("c2 + c3 = %s%n", c2.add(c3));
     }
 }
 
@@ -9,8 +17,7 @@ class Complex {
     final float imag;
 
     Complex() {
-        this.real = 0.0f;
-        this.imag = 0.0f;
+        this(0.0f, 0.0f);
     }
 
     Complex(float real, float imag) {
@@ -24,7 +31,10 @@ class Complex {
 
     @Override
     public String toString() {
-        return String.format("%f + %fi", this.real, this.imag);
+        boolean neg = this.imag < 0;
+
+        return String.format("%f %s %fi", this.real, neg ? "-" : "+",
+            (neg ? -1.0f : 1.0f) * this.imag);
     }
 
     @Override
