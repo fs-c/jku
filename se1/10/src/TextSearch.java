4cd261fe48f9a74aaa68b16923c5ff6d872be3ff
lw <root@fsoc.space>
2019-06-06 11:31:51 +0200

    Refactor to improve structure

--- se1/10/src/TextSearch.java
+++ se1/10/src/TextSearch.java
@@ -1,100 +1,87 @@
-import java.awt.List;
-import java.util.ArrayList;
-
 class Match {
-	int col, row, matching, searchLength;
+	String target, slice;
+	int col, row, matching;
 
-	Match(int col, int row, int matching, int searchLength) {
+	Match(int col, int row, int matching, String target, String slice) {
 		this.col = col;
 		this.row = row;
 		this.matching = matching;
-		this.searchLength = searchLength;
+
+		this.target = target;
+		this.slice = slice;
 	}
 
 	public void print() {
-		Out.format("Found in row %d, column %d, %d/%d characters matched",
-			this.row, this.col, this.matching, this.searchLength);
+		Out.format("Found '%s' in row %d, column %d, %d/%d characters matched%n",
+			slice, this.row, this.col, this.matching, target.length());
+	}
+
+	public static Match findBest(String haystack, String needle) {
+		int col = 1;
+		int row = 1;
+
+		Match best = null;
+
+		for (int i = 0; i < haystack.length() - needle.length(); i++, col++) {
+			char current = haystack.charAt(i);
+
+			if (current == '\n') {
+				row++;
+				col = 0;
+			}
+
+			String slice = Match.getSlice(haystack, i, needle.length());
+			int matching = Match.getMatching(slice, needle);
+
+			if (best == null || best.matching < matching)
+				best = new Match(col, row, matching, needle, slice);
+		}
+
+		return best;
+	}
+
+	private static int getMatching(String source, String target) {
+		int matching = 0;
+
+		for (int j = 0; j < target.length(); j++)
+			if (source.charAt(j) == target.charAt(j))
+				matching++;
+		
+		return matching;
+	}
+
+	private static String getSlice(String source, int offset, int length) {
+		return source.substring(offset, offset + length).toLowerCase();
 	}
 }
 
 class TextSearch {
 	public static void main(String[] args) {
-		String file;
+		String fileName;
 
 		if (args.length > 0) {
-			file = args[0];
+			fileName = args[0];
 		} else {
 			Out.print("File: ");
-			file = In.readWord();
+			fileName = In.readWord();
 		}
 
+		Out.print("Target: ");
 		String target = (In.readLine()).toLowerCase();
 
-		Out.format("searching for '%s' (%d)%n", target, target.length());			
-
-		In.open(file);
-
-		String slice = null;
-
-		int col = 0;
-		int row = 0;
-
-		ArrayList<Match> matches = new ArrayList<Match>();
-
-		while (In.done()) {
-			col++;
-
-			char current = In.read();
-
-			if (current == '\n') {
-				row++;
-				col = 2;
-			}
-
-			int matching = 0;
-			slice = buildSlice(slice, target.length(), current);
-
-			for (int i = 0; i < slice.length(); i++) {
-				if (slice.charAt(i) == target.charAt(i)) {
-					matching++;
-				} else break;
-			}
-
-			matches.add(new Match(col, row, matching, target.length()));
-
-			Out.format("[%d / %d] (%d) - %s%n", col, row, matching, slice);
-		}
-
-		Match best = matches.get(0);
-
-		for (Match m : matches)
-			if (m.matching > best.matching)
-				best = m;
+		String file = readFile(fileName);
+		Match best = Match.findBest(file, target);
 
 		best.print();
 
 		return;
 	}
 
-	static void printChars(char[] chars) {
-		for (int i = 0; i < chars.length; i++) {
-			Out.print(chars[i]);
-		}
+	public String readFile(String file) {
+		In.open(file);
 
-		Out.print('\n');
-	}
+		String content = In.readFile();
 
-	static String buildSlice(String prev, int max, char current) {
-		String slice;
-
-		if (prev == null)
-			return "" + current;
-
-		// If the old one is already full
-		if (max <= prev.length()) {
-			slice = prev.substring(1) + current;
-		} else slice = prev + Character.toLowerCase(current);
-
-		return slice;
+		In.close();
 	}
 }
