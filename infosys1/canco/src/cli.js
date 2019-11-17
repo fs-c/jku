0f614b4cfbc0f2e6d53bde52753286e6db65962a
lw <root@fsoc.space>
2019-11-17 17:00:31 +0100

    Add help option

--- infosys1/canco/src/cli.js
+++ infosys1/canco/src/cli.js
@@ -5,7 +5,27 @@ const { attributeClosure, canonicalCover } = require('./canco');
 
 const [ ,, ...args ] = process.argv;
 
-if (args[0] === 'closure') {
+if ([ '--help', '-h' ].includes(args[0])) {
+    console.log(`
+Usage:
+
+    canco --help/-h
+        Outputs this help message
+
+    canco [functional dependencies]
+        Outputs the canonical cover for the given set of functional dependencies 
+        (A-B,B-CD,D-A)
+
+    canco closure [attributes] [functional dependencies]
+        Outputs the closure of the given set of attributes (A,B,C,...) for the 
+        given set of functional dependencies (A-B,B-CD,D-A).
+
+Set notation:
+
+    Sets are comma (but not space!) delimited strings: A,B,C is recognized but 
+    A, B, C is not
+    `);
+} if (args[0] === 'closure') {
     const deps = stringToDeps(args[2]);
     const attrs = new Set(args[1].split(','));
 
