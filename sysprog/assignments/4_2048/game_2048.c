1269a425a7afd31fa157fa1f0fe7ce0505fe9214
Laurenz Weixlbaumer <root@fsoc.space>
2021-12-11 23:34:26 +0100

    Fix bugs in move_cursor and rewrite allocation

--- sysprog/assignments/4_2048/game_2048.c
+++ sysprog/assignments/4_2048/game_2048.c
@@ -25,11 +25,7 @@ bool has_free_cells(Board *board) {
 	return has_free;
 }
 
-// bool has_possible_moves(Board *board) {
-
-// }
-
-Board *_allocate_board(const isize_t size, ErrorCode *err) {
+Board *allocate_board(const isize_t size, ErrorCode *err) {
 	Board *board = malloc(sizeof(Board));
 
 	if (!board) {
@@ -40,10 +36,8 @@ Board *_allocate_board(const isize_t size, ErrorCode *err) {
 
 	board->size = size;
 
-	isize_t total_cells = board->size * board->size;
-
 	// Allocate a continuous block of memory where cell pointers are stored
-	Cell **cell_ptrs = malloc(total_cells * sizeof(Cell *));
+	Cell **cell_ptrs = malloc(board->size * board->size * sizeof(Cell *));
 
 	if (!cell_ptrs) {
 		*err = EXIT_OUT_OF_MEM;
@@ -79,9 +73,9 @@ Board *_allocate_board(const isize_t size, ErrorCode *err) {
 
 	// Because we allocated the cell pointers as a continuous block we can
 	// refer to a cell by doing
-	// 	(*board->cells + (y * board->size) + x)->value, OR
+	// 	(*(*board->cells + (y * board->size) + x))->value, OR
 	//	board->cells[y][x]->value.
-	// The latter means that the specification was followed properly and the
+	// The latter means that the specification was followed properly while the
 	// former will be useful in move_direction.
 
 	debug("allocated board %lx", (uintptr_t)board);
@@ -89,59 +83,27 @@ Board *_allocate_board(const isize_t size, ErrorCode *err) {
 	return board;
 }
 
-// Allocate a board of the given size (interpreted as size x size cells).
-Board *allocate_board(const isize_t size, ErrorCode *err) {
-	return _allocate_board(size, err);
 
-	// Board *board = malloc(sizeof(Board));
+ErrorCode free_board(Board *board) {
+	if (!board) {
+		return EXIT_INTERNAL;
+	}
 
-	// if (!board) {
-	// 	*err = EXIT_OUT_OF_MEM;
+	for (isize_t i = 0; i < board->size * board->size; i++) {
+		free(*(*board->cells + i));
+	}
 
-	// 	return NULL;
-	// }
+	free(*board->cells);
+	free(board->cells);
+	free(board);
 
-	// board->size = size;
-
-	// // Allocate pointers to the rows (these can be thought of as columns)
-	// board->cells = malloc(size * sizeof(Cell **));
-
-	// if (!board->cells) {
-	// 	*err = EXIT_OUT_OF_MEM;
-
-	// 	return NULL;
-	// }
-
-	// // Allocate pointers to the cells (these can be thought of as rows) and 
-	// // the actual cells themselves
-	// for (isize_t i = 0; i < size; i++) {
-	// 	board->cells[i] = malloc(size * sizeof(Cell *));
-
-	// 	if (!board->cells[i]) {
-	// 		*err = EXIT_OUT_OF_MEM;
-
-	// 		return NULL;
-	// 	}
-
-	// 	// Allocate cells
-	// 	for (isize_t j = 0; j < size; j++) {
-	// 		// Make sure cells have their fields default to zero
-	// 		board->cells[i][j] = calloc(1, sizeof(Cell));
-
-	// 		if (!board->cells[i][j]) {
-	// 			*err = EXIT_OUT_OF_MEM;
-
-	// 			return NULL;
-	// 		}
-	// 	}
-	// }
-
-	// debug("allocated board %lx", (uintptr_t)board);
-
-	// return board;
+	return EXIT_OK;
 }
 
+
 BoardState move_direction(Board *board, Direction dir) {
+	// return _move_direction(board, dir);
+
 	const bool reverse = dir == DIR_DOWN || dir == DIR_RIGHT;
 	const bool horizontal = dir == DIR_LEFT || dir == DIR_RIGHT;
 
@@ -150,49 +112,48 @@ BoardState move_direction(Board *board, Direction dir) {
 
 	// `fixed` represents the row for left/right and column for up/down
 	for (isize_t fixed = 0; fixed < board->size; fixed++) {
-		isize_t prev_merge = 0;
+		// Keep track of where we last moved a value to
+		isize_t cursor = reverse ? board->size - 1 : 0;
 
-		// Start from the direction we're moving to, skip the first cell
-		isize_t i = reverse ? board->size - 1 : 1;
-		for (; reverse ? i > 0 : i <= board->size; reverse ? i-- : i++) {			
-			Cell *cell = horizontal ? board->cells[fixed][i - 1]
-				: board->cells[i - 1][fixed];
+	 	// Start from the direction we're moving to, skip the first cell
+	 	isize_t i = reverse ? board->size - 1 : 1;
+	 	for (; reverse ? i > 0 : i <= board->size; reverse ? i-- : i++) {			
+	 		Cell *cell = horizontal ? board->cells[fixed][i - 1]
+	 			: board->cells[i - 1][fixed];
 
-			// We only care about cells with values in them
-			if (cell->value == 0) {
-				continue;
-			}
+	 		// We only care about cells with values in them
+	 		if (cell->value == 0) {
+	 			continue;
+	 		}
 
-			// Movement direction starting from `i`.
-			const isize_t offset = reverse ? 1 : -1;
+	 		// Movement direction starting from `i`.
+	 		const isize_t offset = reverse ? 1 : -1;
 
-			// A given cell may only merge once per "move"
-			bool has_merged = false;
-			// This prevents 4 2 2 0 -> 8 0 0 0 but 2 2 4 0 -> 8 0 0 0 (wrong!)
+	 		// A given cell may only merge once per "move"
+	 		bool has_merged = false;
+			// To make sure 8 4 4 -> 8 8 0 0.
 
-			// Make cell "bubble" to its new place by iterating
-			// backwards relative to the direction of the outer loop.
-			isize_t j = i - 1;
-			for (; reverse ? j < board->size - 1 : j > 0; j += offset) {
-				Cell *cur = horizontal ? board->cells[fixed][j]
-					: board->cells[j][fixed];
+	 		// Make cell "bubble" to its new place by iterating
+	 		// backwards relative to the direction of the outer loop.
+			// Stop _before_ the last place we moved a value _from_
+			// to make sure 4 4 8 0 -> 8 8 0 0.
+	 		isize_t j = i - 1;
+	 		for (; reverse ? j < cursor : j > cursor; j += offset) {
+	 			Cell *cur = horizontal ? board->cells[fixed][j]
+	 				: board->cells[j][fixed];
 
-				Cell *next = horizontal ? board->cells[fixed][j + offset]
-					: board->cells[j + offset][fixed];
+	 			Cell *next = horizontal ? board->cells[fixed][j + offset]
+	 				: board->cells[j + offset][fixed];
 
-				if (next->value == 0) {
-					next->value = cur->value;
-					cur->value = 0;
-
-					if (has_merged) {
-						prev_merge += offset;
-					}
-				} else if (next->value == cur->value && !has_merged && j != prev_merge) {
-					next->value *= 2;
-					cur->value = 0;
+	 			if (next->value == 0) {
+	 				next->value = cur->value;
+	 				cur->value = 0;
+	 			} else if (next->value == cur->value && !has_merged) {
+	 				next->value *= 2;
+	 				cur->value = 0;
 
 					has_merged = true;
-					prev_merge = j;
+					cursor = j;
 				}
 			}
 		}
@@ -200,23 +161,6 @@ BoardState move_direction(Board *board, Direction dir) {
 
 	return STATE_ONGOING;
 }
-
-BoardState _move_direction(Board *board, Direction dir) {
-	const bool reverse = dir == DIR_DOWN || dir == DIR_RIGHT;
-	const bool horizontal = dir == DIR_LEFT || dir == DIR_RIGHT;
-
-	// `fixed` represents the row for left/right and column for up/down
-	for (isize_t fixed = 0; fixed < board->size; fixed++) {
-		//
-	}
-}
-
-// BoardState _move_direction(Board *board, Direction dir) {
-// 	const bool reverse = dir == DIR_DOWN || dir == DIR_RIGHT;
-// 	const bool horizontal = dir == DIR_LEFT || dir == DIR_RIGHT;
-
-
-// }
 
 // Add 2 or 4 (chosen randomly) to a random position on the given board, 
 // following the required "order of randomness". Returns EXIT_OK even if the 
@@ -272,51 +216,4 @@ Board *create_board(const isize_t size, ErrorCode *err) {
 	*err = add_number(board);
 
 	return board;
-}
-
-ErrorCode _free_board(Board *board) {
-	if (!board) {
-		return EXIT_INTERNAL;
-	}
-
-	for (isize_t i = 0; i < board->size * board->size; i++) {
-		free(*board->cells + i);
-	}
-
-	free(*board->cells);
-	free(board->cells);
-	free(board);
-
-	return EXIT_OK;
-}
-
-ErrorCode free_board(Board *board) {
-	return _free_board(board);
-
-	// if (!board) {
-	// 	return EXIT_INTERNAL;
-	// }
-
-	// for (isize_t i = 0; i < board->size; i++) {
-	// 	for (isize_t j = 0; j < board->size; j++) {
-	// 		if (!board->cells[i][j]) {
-	// 			return EXIT_INTERNAL;
-	// 		}
-
-	// 		free(board->cells[i][j]);
-	// 	}
-
-	// 	if (!board->cells[i]) {
-	// 		return EXIT_INTERNAL;
-	// 	}
-
-	// 	free(board->cells[i]);
-	// }
-
-	// free(board->cells);
-	// free(board);
-
-	// debug("freed board %lx", (uintptr_t)board);
-
-	// return EXIT_OK;
 }
