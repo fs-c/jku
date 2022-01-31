594182e45775fc1b767b4227ba94fa80052b1376
Laurenz Weixlbaumer <root@fsoc.space>
2022-01-31 17:03:24 +0100

    Add some more progress

--- sysprog/assignments/5_matrix/matrixmult.c
+++ sysprog/assignments/5_matrix/matrixmult.c
@@ -9,25 +9,64 @@ typedef struct Matrix {
 	int **data;
 } Matrix;
 
-Matrix *create_matrix(size_t rows, size_t columns) {
+// Creates a matrix (2d array) of the given size. Might still return a pointer to 
+// an incomplete matrix if *err is EXIT_OUT_OF_MEM, make sure to destroy it. 
+Matrix *create_matrix(size_t rows, size_t columns, error_t *err) {
 	Matrix *matrix = malloc(sizeof(Matrix));
+
+	if (!matrix) {
+		*err = EXIT_OUT_OF_MEM;
+
+		return NULL;
+	}
 
 	matrix->rows = rows;
 	matrix->columns = columns;
 
 	matrix->data = malloc(rows * sizeof(int *));
 
+	if (!matrix) {
+		*err = EXIT_OUT_OF_MEM;
+
+		return matrix;
+	}
+
 	for (size_t row = 0; row < rows; row++) {
 		matrix->data[row] = malloc(columns * sizeof(int));
+
+		if (!matrix->data[row]) {
+			*err = EXIT_OUT_OF_MEM;
+
+			return matrix;
+		}
 	}
 
 	return matrix;
 }
 
-error_t destroy_matrix() {
+// Counterpart to create_matrix.
+void destroy_matrix(Matrix *matrix) {
+	if (!matrix) {
+		return;
+	}
 
+	if (matrix->data) {
+		for (size_t row = 0; row < matrix->rows; row++) {
+			if (matrix->data[row]) {
+				free(matrix->data[row]);
+			}
+		}
+
+		free(matrix->data);
+	}
+
+	free(matrix);
 }
 
-int main(int argc, char *argv[]) {
 
+
+int main(int argc, char *argv[]) {
+	error_t error = EXIT_OK;
+
+	Matrix *matrix = create_matrix(0, 0, &error);
 }
