5d54b566daecca87fa29c43510e5c192b5097129
Laurenz Weixlbaumer <root@fsoc.space>
2022-02-14 17:59:19 +0100

    Update testing scripts

--- sysprog/assignments/5_matrix/single_test.sh
+++ sysprog/assignments/5_matrix/single_test.sh
@@ -1,6 +1,6 @@
 make
 
-./matrixmult ref-data/input/$1 > $1.actual
+./matrixmult ref-data/input/$1 &> $1.actual
 
 diff -ys $1.actual ref-data/output/$2
 
--- sysprog/assignments/5_matrix/test.sh
+++ sysprog/assignments/5_matrix/test.sh
@@ -16,20 +16,35 @@ make
 ./matrixmult ref-data/input/wrong_size.in > wrong_size.in.out
 ./matrixmult ref-data/input/zero_matrix.in > zero_matrix.in.out
 
-diff ref-data/output/100x100.out 100x100.in.out
-diff ref-data/output/10x10.out 10x10.in.out
-diff ref-data/output/200x200.out 200x200.in.out
-diff ref-data/output/30x30.out 30x30.in.out
-diff ref-data/output/double_sign.out double_sign.in.out
-diff ref-data/output/incompatible_sizes.out incompatible_sizes.in.out
-diff ref-data/output/input.tout input.txt.out
-diff ref-data/output/large_number.out large_number.in.out
-diff ref-data/output/leading_zero.out leading_zero.in.out
-diff ref-data/output/missing_number.out missing_number.in.out
-diff ref-data/output/random_white_spaces.out random_white_spaces.in.out
-diff ref-data/output/sum_of_squares.out sum_of_squares.in.out
-diff ref-data/output/too_big_number.out too_big_number.in.out
-diff ref-data/output/wrong_size.out wrong_size.in.out
-diff ref-data/output/zero_matrix.out zero_matrix.in.out
+echo "100x100.in"
+diff -ys ref-data/output/100x100.out 100x100.in.out
+echo "10x10.in"
+diff -ys ref-data/output/10x10.out 10x10.in.out
+echo "200x200.in"
+diff -ys ref-data/output/200x200.out 200x200.in.out
+echo "30x30.in"
+diff -ys ref-data/output/30x30.out 30x30.in.out
+echo "double_sign.in"
+diff -ys ref-data/output/double_sign.out double_sign.in.out
+echo "incompatible_sizes.in"
+diff -ys ref-data/output/incompatible_sizes.out incompatible_sizes.in.out
+echo "input.txt"
+diff -ys ref-data/output/input.tout input.txt.out
+echo "large_number.in"
+diff -ys ref-data/output/large_number.out large_number.in.out
+echo "leading_zero.in"
+diff -ys ref-data/output/leading_zero.out leading_zero.in.out
+echo "missing_number.in"
+diff -ys ref-data/output/missing_number.out missing_number.in.out
+echo "random_white_spaces.in"
+diff -ys ref-data/output/random_white_spaces.out random_white_spaces.in.out
+echo "sum_of_squares.in"
+diff -ys ref-data/output/sum_of_squares.out sum_of_squares.in.out
+echo "too_big_number.in"
+diff -ys ref-data/output/too_big_number.out too_big_number.in.out
+echo "wrong_size.in"
+diff -ys ref-data/output/wrong_size.out wrong_size.in.out
+echo "zero_matrix.in"
+diff -ys ref-data/output/zero_matrix.out zero_matrix.in.out
 
 make clean
