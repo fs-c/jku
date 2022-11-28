2aae29fedf98857e66112b9782e02f55a9c67b10
Laurenz Weixlbaumer <root@fsoc.space>
2022-11-28 12:31:32 +0100

    Add some missing changes

--- 2022w/algodat2/03/ChainingHashSet.java
+++ 2022w/algodat2/03/ChainingHashSet.java
@@ -18,7 +18,7 @@ public class ChainingHashSet implements IHashSet {
 		this.hashTable = table;
 		this.capacity = table.length;
 		this.size = 0;
-		for(ChainingHashNode node : table) {
+		for (ChainingHashNode node : table) {
 			while(node != null) {
 				this.size++;
 				node = node.next;
@@ -33,42 +33,122 @@ public class ChainingHashSet implements IHashSet {
 
 	@Override
 	public ChainingHashNode[] getHashTable() {
-		// ...
-		return null;
+		return this.hashTable;
 	}
 
 	@Override
 	public int size() {
-		// ...
-		return -1;
+		return size;
 	}
 
 	@Override
 	public boolean insert(int key) {
-		// ...
-		return false;
+		int hash = getHashCode(key);
+
+		if (hashTable[hash] == null) {
+			hashTable[hash] = new ChainingHashNode(key);
+		} else {
+			var node = hashTable[hash];
+
+			if (node.next == null && node.key == key) {
+				return false;
+			}
+
+			while (node.next != null) {
+				if (node.key == key) {
+					return false;
+				}
+
+				node = node.next;
+			}
+
+			node.next = new ChainingHashNode(key);
+		}
+
+		size++;
+
+		return true;
 	}
 
 	@Override
 	public boolean contains(int key) {
-		// ...
+		int hash = getHashCode(key);
+
+		if (hashTable[hash] != null) {
+			var node = hashTable[hash];
+			do {
+				if (node.key == key) {
+					return true;
+				}
+
+				node = node.next;
+			} while (node != null);
+		}
+
 		return false;
 	}
 
 	@Override
 	public boolean remove(int key) {
-		// ...
+		int hash = getHashCode(key);
+
+		if (hashTable[hash] != null) {
+			var node = hashTable[hash];
+
+			if (node.key == key) {
+				hashTable[hash] = node.next;
+				size--;
+				return true;
+			}
+
+			while (node.next != null) {
+				if (node.next.key == key) {
+					node.next = node.next.next;
+					size--;
+					return true;
+				}
+
+				node = node.next;
+			}
+		}
+
 		return false;
 	}
 
 	@Override
 	public void clear() {
-		// ...
+		for (int i = 0; i < capacity; i++) {
+			hashTable[i] = null;
+		}
+
+		size = 0;
 	}
 
 	@Override
 	public String toString() {
-		// ...
-		return "";
+		var sb = new StringBuilder();
+
+		for (int i = 0; i < capacity; i++) {
+			sb.append(i).append(" {");
+
+			var node = hashTable[i];
+			while (node != null) {
+				sb.append(node.key);
+
+				if (node.next != null) {
+					sb.append(", ");
+				}
+
+				node = node.next;
+			}
+
+			sb.append("}");
+
+			if (i < capacity - 1) {
+				sb.append(", ");
+			}
+		}
+
+		return sb.toString();
 	}
 }
