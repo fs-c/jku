b9bf429b6c9c35e7c5425ec9f6f8b63bae53ade6
Laurenz Weixlbaumer <root@fsoc.space>
2023-03-18 11:32:05 +0100

    Consistent tab formatting

--- 2023s/arch/01/1.s
+++ 2023s/arch/01/1.s
@@ -6,40 +6,40 @@
 main:
 
 a:
-	# beim left shift ist nichts besonderes zu beachten/beobachten.
+    # beim left shift ist nichts besonderes zu beachten/beobachten.
     # beim right shift wurde ein arithmetic right shift verwendet weil
     # integer signed ist. hier wird das MSB über alle "leeren" positionen
     # kopiert (sign extension).
 
-	lui a0, 0xE			# a0 = 0xE000, 11th bit of 0xDD44 is set so increase by one
-    addi a0, a0, 0xDD4	# a0 = 0xDD44
+    lui a0, 0xE         # a0 = 0xE000, 11th bit of 0xDD44 is set so increase by one
+    addi a0, a0, 0xDD4  # a0 = 0xDD44
     
-    lui a1, 0x10		# a1 = 0x10000, see above
-    addi a1, a1, 0xFFF	# a1 = 0xFFFF
+    lui a1, 0x10        # a1 = 0x10000, see above
+    addi a1, a1, 0xFFF  # a1 = 0xFFFF
     
     # (((((a0 / 0xEE) + 0xD) << 0x4) << 20) >> 0x14) & a1
-    addi t0, zero, 0xEE	# temp var for division
-    div a0, a0, t0		# a0 /= 0xEE
-    addi a0, a0, 0xD	# a0 += 0xD
-    slli a0, a0, 0x4	# a0 <<= 0x4
-    slli a0, a0, 20		# a0 <<= 20
-    srai a0, a0, 0x14	# a0 >>= 0x14
-    and a0, a0, a1		# a0 &= a1
+    addi t0, zero, 0xEE # temp var for division
+    div a0, a0, t0      # a0 /= 0xEE
+    addi a0, a0, 0xD    # a0 += 0xD
+    slli a0, a0, 0x4    # a0 <<= 0x4
+    slli a0, a0, 20     # a0 <<= 20
+    srai a0, a0, 0x14   # a0 >>= 0x14
+    and a0, a0, a1      # a0 &= a1
     
 b:
-	# long long ist immer ein 64-bit integer, long ist entweder 32- oder 64-bit
+    # long long ist immer ein 64-bit integer, long ist entweder 32- oder 64-bit
     # abhängig von der architektur (RV32 oder RV64), unterscheiden sich also nur 
     # für RV32. void foo(long long param) unter RV32 bekommt param in a0 und a1, 
     # unter RV64 in a0.
     
 c:
-	lui a0, 0x30400		# a0 = 0x30400000, +1 again because 11th bit is set
-    addi a0, a0, 0xB80	# a0 = 0x303FFB80
-    addi t0, zero, 0	# loop counter
-    addi t1, zero, 6	# max iterations
+	lui a0, 0x30400     # a0 = 0x30400000, +1 again because 11th bit is set
+    addi a0, a0, 0xB80  # a0 = 0x303FFB80
+    addi t0, zero, 0    # loop counter
+    addi t1, zero, 6    # max iterations
 loop:
-	srai a0, a0, 1		# a0 >>= 1
-    addi t0, t0, 1		# i++
-    blt t0, t1, loop	# continue loop if counter < 6
+    srai a0, a0, 1      # a0 >>= 1
+    addi t0, t0, 1      # i++
+    blt t0, t1, loop    # continue loop if counter < 6
 end:
     
--- 2023s/arch/01/2.s
+++ 2023s/arch/01/2.s
@@ -5,23 +5,23 @@ array: .word 8, 6, 3, 4, 0, 1, 2, 7
 
 .text
 main:
-	addi t0, zero, 8	# t0 = array length
-    addi t1, zero, 0	# n
-    addi t2, zero, 1	# temp var for shift
-    sra t0, t0, t2		# t0 = max iterations (sra by 1 to div by 2)
-    la a0, array		# a0 = array
+    addi t0, zero, 8    # t0 = array length
+    addi t1, zero, 0    # n
+    addi t2, zero, 1    # temp var for shift
+    sra t0, t0, t2      # t0 = max iterations (sra by 1 to div by 2)
+    la a0, array        # a0 = array
 loop:
-	addi a1, a0, 4		# a1 = a0 + 1
+    addi a1, a0, 4      # a1 = a0 + 1
 
-	lw t2, 0(a0)		# t2 = *a0
-    lw t3, 0(a1)		# t3 = *a1
-    sw t2, 0(a1)		# *a1 = t2
-    sw t3, 0(a0)		# *a0 = t3
+    lw t2, 0(a0)        # t2 = *a0
+    lw t3, 0(a1)        # t3 = *a1
+    sw t2, 0(a1)        # *a1 = t2
+    sw t3, 0(a0)        # *a0 = t3
 
-    addi a0, a0, 8		# a0 += 2
+    addi a0, a0, 8      # a0 += 2
 
-	addi t1, t1, 1		# n++
-    blt t1, t0, loop	# continue loop if n < max iterations
+    addi t1, t1, 1      # n++
+    blt t1, t0, loop    # continue loop if n < max iterations
 end:
 
 # bei ungerader laenge bleibt das letzte element unveraendert weil len/2
