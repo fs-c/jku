959274635f7f449bdd5f2f35c862629a0c341de7
fsoc <root@fsoc.space>
2023-12-10 20:05:33 +0100

    Finish A4

--- 2023w/ai/ai_assignment4/ai_assignments/decision_tree/id3.py
+++ 2023w/ai/ai_assignment4/ai_assignments/decision_tree/id3.py
@@ -31,18 +31,25 @@ class DecisionTreeNode(DecisionTreeNodeBase):
             self.features = self.features[idx_sort, :] # it makes a difference if features is overwritten or not (sorted differently)
             self.labels = self.labels[idx_sort]
 
-            # TODO: check for consecutive samples whether the labels and features are different
-            # be careful to not compare the 0th sample with the last sample when indexing
-            # compute splitting values and add to list: split_points.append((f_idx, split_at))
+            for i in range(1, nr_samples):
+                if self.labels[i] != self.labels[i - 1] and self.features[i, f_idx] != self.features[i - 1, f_idx]:
+                    split_at = (self.features[i, f_idx] + self.features[i - 1, f_idx]) / 2
+                    split_points.append((f_idx, split_at))
 
         return split_points
 
     def get_optimal_split_point(self):
         split_feature, split_point = None, None
 
-        # TODO: 
-        # 1. get all possible split points (self.get_all_possible_split_points())
-        # 2. loop over all possible split points that you computed and return the best one
+        max_info_gain = -float('inf')
+        all_split_points = self.get_all_possible_split_points()
+
+        for f_idx, split_at in all_split_points:
+            information_gain = self.compute_information_gain(self.features[:, f_idx], split_at)
+            if information_gain > max_info_gain:
+                max_info_gain = information_gain
+                split_feature = f_idx
+                split_point = split_at
 
         return split_feature, split_point
 
@@ -61,20 +68,30 @@ class DecisionTreeNode(DecisionTreeNodeBase):
         return e_data - (weight_left * e_left + weight_right * e_right)
 
     def split(self):
-        # TODO: implement the ID3 algorithm based on the lecture slides "The ID3 Algorithm"
         # 1. If all examples X in the current node belong to the same class,
         #    make current node a leaf, label with class (use unique_unsorted)
+        if len(unique_unsorted(self.labels)) == 1:
+            self.label = self.labels[0]
+            return
 
         # 2. use get_optimal_split_point() to determine best feature for splitting
+        split_feature, split_point = self.get_optimal_split_point()
+        self.split_feature = split_feature
+        self.split_point = split_point
 
         # 3. + 4. Create a branch + successor node N_i for each value of the selected feature 
         #         containing a subset of X 
         #         - X <= split_point goes into left_child
         #         - X > split_point goes into right_child
-        
-        # 5. For each subnode split is called recursively
+        left_features = self.features[self.features[:, split_feature] <= split_point]
+        left_labels = self.labels[self.features[:, split_feature] <= split_point]
 
-        pass # placeholder
+        right_features = self.features[self.features[:, split_feature] > split_point]
+        right_labels = self.labels[self.features[:, split_feature] > split_point]
+
+        # 5. For each subnode split is called recursively
+        self.left_child = DecisionTreeNode(left_features, left_labels)
+        self.right_child = DecisionTreeNode(right_features, right_labels)
 
 class ID3():
     def __init__(self):
