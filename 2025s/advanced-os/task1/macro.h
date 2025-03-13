9bf94be049bec6c61dc051478c6a8ef192a4c0d2
fsoc <root@fsoc.space>
2025-03-13 22:36:30 +0100

    Add partial advos task 1

--- 2025s/advanced-os/task1/macro.h
+++ 2025s/advanced-os/task1/macro.h
@@ -1,23 +1,19 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#include <linux/kernel.h>
-#include <linux/string.h>
-#include <linux/ktime.h>
-#include <linux/kobject.h>
 #include <linux/input.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+#include <linux/ktime.h>
+#include <linux/string.h>
 
 struct macro {
-	// The key to press for replacement
-	// Take care: this is the keyboard position code, not the key printed on it!
-	// E.g. 'a' is 30
-	char replacer;
-
-	// The replacement string; for simplicity a fixed-length array
-	char replacement[20];
+    // keypress to replace, see https://elixir.bootlin.com/linux/v6.13.6/source/include/uapi/linux/input-event-codes.h#L64
+    char to_replace;
+    // replacement string
+    char replacement[20];
 };
 
-// Note: No '\n' at the end. You must therefore use 'echo -n "Off" > ...' instead of 'echo "Off" > /sys/kernel/macro/state'
 #define ON "On"
 #define OFF "Off"
 
--- 2025s/advanced-os/task1/macro_main.c
+++ 2025s/advanced-os/task1/macro_main.c
@@ -1,58 +1,226 @@
-#include "macro.h"
-#include <linux/slab.h>	
 #include <linux/module.h>
 #include <linux/random.h>
+#include <linux/slab.h>
+#include <linux/workqueue.h>
+
+#include "macro.h"
 
 MODULE_DESCRIPTION("Macro replacer.");
 MODULE_AUTHOR("lw");
 MODULE_LICENSE("GPL");
 
-#define DEVICE_NAME "AT Translated Set 2 keyboard" // --> Produces solely EV_ABS events (no clicks, absolute positioning)
+#define DEVICE_NAME "AT Translated Set 2 keyboard"
 
-// See https://elixir.bootlin.com/linux/v6.13.6/source/include/uapi/linux/input-event-codes.h#L64 for numbers
-// The caodes are #define'd, so here we have to translate this MANUALLY!
-struct macro macro_list[]={
-	{35,"\x23\x12\x26\x26\x18\x39\x11\x18\x13\x26\x20"},	// h -> "hello world"; note: "H" is difficult, as it requires 
-						// four "keys": Shift-Down + h-down + h-up + shift-up
-	{30,"\x05"}, 	// a -> 4
-	{18,"\x04"},	// e -> 3
-	{23,"\x02"},	// i -> 1
-	{24,"\x0b"},	// o -> 0
-	{0,""}		// code 0 means end of list!
+#define KEY_PRESS_TIME 50
+#define TIME_BETWEEN_KEYS 50
+
+enum timer_work_state {
+    TIMER_WORK_KEYDOWN,
+    TIMER_WORK_KEYUP,
 };
 
-/* What kind of device ids this filter handles
- * input_register_device will only try to connect us to devices
- * which match this filter, i.e. provide these kinds of events.
- * Lots of filtering possible: see linux/include/linux/mod_devicetable.h
- * We are only interested in things that produce mouse events.
- */
-static const struct input_device_id idsOfInterest[] = {
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_KEY) },	// Keypresses
-	},
-	{ },
+struct timer_work_unit {
+    enum timer_work_state state;
+    int index;
+    struct macro* macro;
+    struct input_handle* handle;
 };
 
-/* Used to generate information used for hot-plugging and map files.
- * Not strictly needed here, as we don't do any of these things: manual
- * loading only. The mouse wiggler also works without.
- */
-MODULE_DEVICE_TABLE(input,idsOfInterest);
+static struct hrtimer timer;
+static struct timer_work_unit timer_work_unit;
 
+static enum hrtimer_restart timer_work_callback(struct hrtimer* timer)
+{
+    if (timer_work_unit.macro == NULL || timer_work_unit.handle == NULL) {
+        printk(KERN_ERR "macro: timer_work_callback: macro or handle is NULL (%p, %p)\n", timer_work_unit.macro, timer_work_unit.handle);
+        return HRTIMER_NORESTART;
+    }
 
-// Module initialization
-static int __init macro_init(void) {
-	
+    char key = timer_work_unit.macro->replacement[timer_work_unit.index];
+    if (!key) {
+        printk(KERN_DEBUG "macro: finished sending macro\n");
+        return HRTIMER_NORESTART;
+    }
+
+    if (timer_work_unit.state == TIMER_WORK_KEYDOWN) {
+        // send fake keydown
+        input_report_key(timer_work_unit.handle->dev, key, 1);
+        input_sync(timer_work_unit.handle->dev);
+        printk(KERN_DEBUG "macro: sent fake keydown %d\n", key);
+
+        // schedule keyup
+        timer_work_unit.state = TIMER_WORK_KEYUP;
+        hrtimer_forward_now(timer, ms_to_ktime(KEY_PRESS_TIME));
+    } else if (timer_work_unit.state == TIMER_WORK_KEYUP) {
+        // send fake keyup
+        input_report_key(timer_work_unit.handle->dev, key, 0);
+        input_sync(timer_work_unit.handle->dev);
+        printk(KERN_DEBUG "macro: sent fake keyup %d\n", key);
+
+        // move to next key
+        timer_work_unit.index++;
+        timer_work_unit.state = TIMER_WORK_KEYDOWN;
+        hrtimer_forward_now(timer, ms_to_ktime(TIME_BETWEEN_KEYS));
+    }
+
+    return HRTIMER_RESTART;
 }
 
-// Module cleanup; we simply unregister our handler
-static void __exit macro_cleanup(void) {
-	...
+struct macro macro_list[] = {
+    // "hello world"
+    { 35, "\x23\x12\x26\x26\x18\x39\x11\x18\x13\x26\x20" },
+    // "a"
+    { 30, "\x05" },
+    // "e"
+    { 18, "\x04" },
+    // "i"
+    { 23, "\x02" },
+    // "o"
+    { 24, "\x0b" },
+    { 0, "" }
+};
+
+static struct macro* get_macro_for_key(unsigned int code)
+{
+    for (int i = 0; macro_list[i].to_replace != 0; i++) {
+        if (macro_list[i].to_replace == code) {
+            return &macro_list[i];
+        }
+    }
+
+    return NULL;
 }
 
-// Module initialization/finalization functions
+static int on_connect(struct input_handler* handler, struct input_dev* dev,
+    const struct input_device_id* id)
+{
+    printk(KERN_DEBUG "macro: got on_connect to %s\n", dev->name);
+
+    if (strcasecmp(dev->name, DEVICE_NAME)) {
+        return 0;
+    }
+
+    struct input_handle handle = {
+        .dev = dev,
+        .handler = handler,
+        .name = "macro",
+    };
+
+    int res = input_register_handle(&handle);
+    if (res) {
+        printk(KERN_ERR "macro: failed to register input handle: %d\n", res);
+        return res;
+    }
+
+    res = input_open_device(&handle);
+    if (res) {
+        printk(KERN_ERR "macro: failed to open input device: %d\n", res);
+        input_unregister_handle(&handle);
+        return res;
+    }
+
+    printk(KERN_INFO "macro: connected to %s\n", dev->name);
+
+    return 0;
+}
+
+static void on_disconnect(struct input_handle* handle)
+{
+    if (handle == NULL) {
+        printk(KERN_ERR "macro: on_disconnect: handle is NULL\n");
+        return;
+    }
+
+    printk(KERN_INFO "macro: disconnecting from %s\n", handle->dev->name);
+
+    input_close_device(handle);
+    input_unregister_handle(handle);
+    handle->dev = NULL;
+    handle->handler = NULL;
+}
+
+static bool on_filter(struct input_handle* handle, unsigned int type,
+    unsigned int code, int value)
+{
+    printk(KERN_DEBUG "macro: on_filter %d %d %d\n", type, code, value);
+
+    if (type != EV_KEY) {
+        // todo: this case happens pretty often but i don't know why
+        return false;
+    }
+
+    // if we are currently sending a macro, don't start a new one
+    if (hrtimer_active(&timer)) {
+        printk(KERN_DEBUG "macro: timer is active, skipping\n");
+        return false;
+    }
+
+    struct macro* macro = get_macro_for_key(code);
+    if (macro == NULL) {
+        printk(KERN_DEBUG "macro: no macro found for key %d\n", code);
+        return false;
+    }
+
+    printk(KERN_DEBUG "macro: found macro for key %d: '%s'\n", code, macro->replacement);
+
+    switch (value) {
+    case 0:
+        // keyup, start sending the macro and swallow
+        timer_work_unit.index = 0;
+        timer_work_unit.state = TIMER_WORK_KEYDOWN;
+        timer_work_unit.macro = macro;
+        timer_work_unit.handle = handle;
+        hrtimer_start(&timer, ms_to_ktime(0), HRTIMER_MODE_REL);
+        return true;
+    case 1:
+        // keydown, swallow but only send the macro on keyup
+        return true;
+    }
+
+    return false;
+}
+
+static const struct input_device_id inputs_to_handle[] = {
+    // EV_KEY = keypress, 1 on keydown, 0 on keyup
+    // see https://www.kernel.org/doc/Documentation/input/event-codes.txt
+    {
+        .flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+        .evbit = { BIT_MASK(EV_KEY) },
+    },
+    {},
+};
+
+static struct input_handler input_handler = {
+    .name = "macro-input-handler",
+    .connect = on_connect,
+    .disconnect = on_disconnect,
+    .filter = on_filter,
+    .id_table = inputs_to_handle,
+};
+
+static int __init macro_init(void)
+{
+    // register handler
+    int res = input_register_handler(&input_handler);
+    if (res) {
+        printk(KERN_ERR "macro: failed to register input handler: %d\n", res);
+        return res;
+    }
+
+    hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    timer.function = timer_work_callback;
+
+    printk(KERN_INFO "macro: init done\n");
+    return 0;
+}
+
+static void __exit macro_cleanup(void)
+{
+    input_unregister_handler(&input_handler);
+    hrtimer_cancel(&timer);
+
+    printk(KERN_INFO "macro: cleanup done\n");
+}
+
 module_init(macro_init);
 module_exit(macro_cleanup);
-
