61e8284af29b2d6eb2fd6706c9ccc350130d1f35
Laurenz Weixlbaumer <root@fsoc.space>
2025-05-11 10:47:57 +0200

    Reformat & fix some compile errors

--- 2025s/advanced-os/task2/kbd_buffer.c
+++ 2025s/advanced-os/task2/kbd_buffer.c
@@ -25,149 +25,142 @@ static struct proc_dir_entry *proc_entry;
 static int write_pos = 0;
 static int read_pos = 0;
 
-static int myConnect(struct input_handler *handler, struct input_dev *dev,
-                     const struct input_device_id *id)
+static int logger_connect(struct input_handler *handler, struct input_dev *dev,
+			  const struct input_device_id *id)
 {
-    int error;
-    if (strcasecmp(dev->name, DEVICE_NAME))
-    {
-        return 0;
-    }
-    printk(KERN_INFO "Connect to device \"%s\"\n", dev->name);
-    handle.dev = dev;
-    handle.handler = handler;
-    handle.name = "logger";
-    error = input_register_handle(&handle);
-    if (error)
-    {
-        printk(KERN_ERR "Could not register handle\n");
-        return error;
-    }
-    error = input_open_device(&handle);
-    if (error)
-    {
-        input_unregister_handle(&handle);
-        printk(KERN_ERR "Could not open handle\n");
-        return error;
-    }
-    return 0;
+	int error;
+
+	if (strcasecmp(dev->name, DEVICE_NAME)) {
+		return 0;
+	}
+
+	printk(KERN_INFO "connect to device \"%s\"\n", dev->name);
+	handle.dev = dev;
+	handle.handler = handler;
+	handle.name = "logger";
+	error = input_register_handle(&handle);
+	if (error) {
+		printk(KERN_ERR "could not register handle\n");
+		return error;
+	}
+
+	error = input_open_device(&handle);
+	if (error) {
+		input_unregister_handle(&handle);
+		printk(KERN_ERR "could not open handle\n");
+		return error;
+	}
+
+	return 0;
 }
 
-static void myDisconnect(struct input_handle *handle)
+static void logger_disconnect(struct input_handle *handle)
 {
-    if (handle == NULL)
-    {
-        return;
-    }
-    printk(KERN_INFO "Disconnect from device \"%s\"\n", handle->dev->name);
-    input_close_device(handle);
-    input_unregister_handle(handle);
-    handle->dev = NULL;
-    handle->handler = NULL;
+	if (handle == NULL) {
+		return;
+	}
+
+	printk(KERN_INFO "disconnect from device \"%s\"\n", handle->dev->name);
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	handle->dev = NULL;
+	handle->handler = NULL;
 }
 
-static bool myFilter(struct input_handle *handle, unsigned int type,
-                     unsigned int code, int value)
+static bool logger_filter(struct input_handle *handle, unsigned int type,
+			  unsigned int code, int value)
 {
-    if (type == EV_KEY && value == 1)
-    { // Only store key presses
-        mutex_lock(&buffer_mutex);
-        buffer[write_pos] = (unsigned char)code;
-        write_pos = (write_pos + 1) % BUFFER_SIZE;
-        if (data_count < BUFFER_SIZE)
-        {
-            data_count++;
-        }
-        else
-        {
-            read_pos = (read_pos + 1) % BUFFER_SIZE;
-        }
-        mutex_unlock(&buffer_mutex);
-    }
-    return false;
+	if (type != EV_KEY || value == 1) {
+		return false;
+	}
+
+	mutex_lock(&buffer_mutex);
+	buffer[write_pos] = (unsigned char)code;
+	write_pos = (write_pos + 1) % BUFFER_SIZE;
+	if (data_count < BUFFER_SIZE) {
+		data_count++;
+	} else {
+		read_pos = (read_pos + 1) % BUFFER_SIZE;
+	}
+	mutex_unlock(&buffer_mutex);
 }
 
-static ssize_t proc_read(struct file *file, char __user *buffer, size_t count, loff_t *ppos)
+static ssize_t proc_read(struct file *file, char __user *buffer, size_t count,
+			 loff_t *ppos)
 {
-    if (*ppos > 0)
-    {
-        return 0;
-    }
+	if (*ppos > 0) {
+		return 0;
+	}
 
-    int bytes_read = 0;
-    unsigned char temp_buffer[4];
-    int bytes_to_read = min(min((int)count, 4), data_count);
+	unsigned char temp_buffer[4];
+	int bytes_to_read = min(min((int)count, 4), data_count);
 
-    mutex_lock(&buffer_mutex);
-    for (int i = 0; i < bytes_to_read; i++)
-    {
-        temp_buffer[i] = buffer[read_pos];
-        read_pos = (read_pos + 1) % BUFFER_SIZE;
-        data_count--;
-    }
+	mutex_lock(&buffer_mutex);
+	for (int i = 0; i < bytes_to_read; i++) {
+		temp_buffer[i] = buffer[read_pos];
+		read_pos = (read_pos + 1) % BUFFER_SIZE;
+		data_count--;
+	}
 
-    if (copy_to_user(buffer, temp_buffer, bytes_to_read))
-    {
-        return -EFAULT;
-    }
-    mutex_unlock(&buffer_mutex);
+	if (copy_to_user(buffer, temp_buffer, bytes_to_read)) {
+		return -EFAULT;
+	}
+	mutex_unlock(&buffer_mutex);
 
-    *ppos += bytes_to_read;
-    return bytes_to_read;
+	*ppos += bytes_to_read;
+	return bytes_to_read;
 }
 
-static const struct file_operations proc_fops = {
-    .read = proc_read,
+static const struct proc_ops proc_fops = {
+	.read = proc_read,
 };
 
-static const struct input_device_id idsOfInterest[] = {
-    {
-        .flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-        .evbit = {BIT_MASK(EV_KEY)},
-    },
-    {},
+static const struct input_device_id relevant_device_ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) },
+	},
+	{},
 };
 
-MODULE_DEVICE_TABLE(input, idsOfInterest);
+MODULE_DEVICE_TABLE(input, relevant_device_ids);
 
-static struct input_handler inputHandler = {
-    .name = "keylogger-handler",
-    .connect = myConnect,
-    .disconnect = myDisconnect,
-    .filter = myFilter,
-    .id_table = idsOfInterest,
+static struct input_handler logger_handler = {
+	.name = "keylogger-handler",
+	.connect = logger_connect,
+	.disconnect = logger_disconnect,
+	.filter = logger_filter,
+	.id_table = relevant_device_ids,
 };
-
-static int logger_init(void)
-{
-    mutex_init(&buffer_mutex);
-
-    // 0444 = read only for all groups
-    proc_entry = proc_create(PROC_FS_NAME, 0444, NULL, &proc_fops);
-    if (proc_entry == NULL)
-    {
-        printk(KERN_ERR "could not create proc entry\n");
-        return -ENOMEM;
-    }
-
-    int res = input_register_handler(&inputHandler);
-    if (res)
-    {
-        printk(KERN_ERR "could not register input handler: %d\n", res);
-        logger_cleanup();
-        return res;
-    }
-
-    printk(KERN_INFO "keyboard buffer module loaded successfully\n");
-    return 0;
-}
 
 static void logger_cleanup(void)
 {
-    input_unregister_handler(&inputHandler);
-    proc_remove(proc_entry);
-    mutex_destroy(&buffer_mutex);
-    printk(KERN_INFO "keyboard buffer module unloaded\n");
+	input_unregister_handler(&logger_handler);
+	proc_remove(proc_entry);
+	mutex_destroy(&buffer_mutex);
+	printk(KERN_INFO "keyboard buffer module unloaded\n");
+}
+
+static int logger_init(void)
+{
+	mutex_init(&buffer_mutex);
+
+	// 0444 = read only for all groups
+	proc_entry = proc_create(PROC_FS_NAME, 0444, NULL, &proc_fops);
+	if (proc_entry == NULL) {
+		printk(KERN_ERR "could not create proc entry\n");
+		return -ENOMEM;
+	}
+
+	int res = input_register_handler(&logger_handler);
+	if (res) {
+		printk(KERN_ERR "could not register input handler: %d\n", res);
+		logger_cleanup();
+		return res;
+	}
+
+	printk(KERN_INFO "keyboard buffer module loaded successfully\n");
+	return 0;
 }
 
 module_init(logger_init);
--- 2025s/advanced-os/task2/read_buffer.c
+++ 2025s/advanced-os/task2/read_buffer.c
@@ -8,37 +8,32 @@
 
 int main()
 {
-    unsigned char buffer[MAX_READ];
+	unsigned char buffer[MAX_READ];
 
-    const int fd = open(PROC_PATH, O_RDONLY);
-    if (fd == -1)
-    {
-        perror("error opening proc file");
-        return 1;
-    }
+	const int fd = open(PROC_PATH, O_RDONLY);
+	if (fd == -1) {
+		perror("error opening proc file");
+		return 1;
+	}
 
-    while (true)
-    {
-        const ssize_t bytes_read = read(fd, buffer, MAX_READ);
-        if (bytes_read <= 0)
-        {
-            break;
-        }
+	while (true) {
+		const ssize_t bytes_read = read(fd, buffer, MAX_READ);
+		if (bytes_read <= 0) {
+			break;
+		}
 
-        for (int i = 0; i < bytes_read; i++)
-        {
-            printf("%d ", buffer[i]);
-        }
-        printf("\n");
+		for (int i = 0; i < bytes_read; i++) {
+			printf("%d ", buffer[i]);
+		}
+		printf("\n");
 
-        if (bytes_read < MAX_READ)
-        {
-            break;
-        }
+		if (bytes_read < MAX_READ) {
+			break;
+		}
 
-        sleep(2);
-    }
+		sleep(2);
+	}
 
-    close(fd);
-    return 0;
+	close(fd);
+	return 0;
 }
